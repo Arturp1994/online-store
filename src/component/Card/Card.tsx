224eb3b435d1c13d30b38aa5b71eeee507bbffbe
Arthur <arturiopr1994@gmail.com>
2022-07-15 12:08:09 +0300

    3 all

--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import style from './Card.module.scss'
+
 
 type CarsPropsType = {
     title: string,
@@ -11,21 +13,21 @@ export const Card = (props: CarsPropsType) => {
 
 
     return (
-        <div className={'card'}>
-            <div className={'favorite'}>
+        <div className={style.card}>
+            <div className={'style.favorite'}>
                 <img width={18} height={15} src={'/img/Iconnotlike.png'} alt={'notLike'}/>
             </div>
 
             <img width={133} height={112}
                  src={props.imageURL} alt={'photo'}/>
             <h5>{props.title}</h5>
-            <div className={'cardButton'}>
+            <div className={style.cardButton}>
                 <div>
                     <span>Цена:</span>
                     <b>{props.price} руб.</b>
                 </div>
                 <div>
-                    <button className={'button'} onClick={props.onClick}>
+                    <button className={style.button} onClick={props.onClick}>
                         <img width={11} height={11} src='/img/Icon.svg'/>
                     </button>
                 </div>
