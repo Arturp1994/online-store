ecb7bfd7b8e14180a5dba84f2d4783ecd98776dc
Arthur <arturiopr1994@gmail.com>
2022-07-17 12:44:29 +0300

    5 add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import axios from "axios";
 import './App.css';
 import {Card} from "./component/Card/Card";
 import {Header} from "./component/Header/Header";
@@ -16,15 +17,18 @@ function App() {
     const [items, setItems] = useState<ArrType>([])
     const [cardItems, setCardItems] = useState<ArrType>([])
     const [cardOpened, setCardOpened] = useState(false)
+    const [searchValue, setSearchValue] = useState('')
 
-    useEffect(()=>{
-        fetch('https://62d145addccad0cf176431e2.mockapi.io/Items').then(res =>{
-            return res.json()
-        }).then((json)=>{setItems(json)})
+    useEffect(() => {
+        axios.get('https://62d145addccad0cf176431e2.mockapi.io/Items').then((res)=>{setItems(res.data)})
     }, [])
 
-    const onAddToCard= (name: string,price: number,imageURL: string) => {
-        setCardItems([...cardItems, {name,price,imageURL}])
+    const onAddToCard = (name: string, price: number, imageURL: string) => {
+        setCardItems([...cardItems, {name, price, imageURL}])
+    }
+
+    const onChangeSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.currentTarget.value)
     }
 
     return (
@@ -39,25 +43,28 @@ function App() {
             }}/>
             <div className='content'>
                 <div className='all'>
-                    <h1>Все товары</h1>
+                    <h1>{searchValue ? `Поиск по запросу: '${searchValue}'` : 'Все товары'}</h1>
                     <div className='search-block'>
                         <div><img width={25} height={25}
                                   src={"/img/search.png"}
                                   alt={'Search'}/></div>
-                        <div><input placeholder={'Поиск...'}/></div>
+                        <div><input placeholder={'Поиск...'} onChange={onChangeSearchInput}/></div>
                     </div>
                 </div>
                 <div className='all-technic'>
 
-                    {items.map(card => (
-                        <Card title={card.name}
-                              price={card.price}
-                              imageURL={card.imageURL}
-                              onClickPlus={(st, n, img)=>onAddToCard(st,n,img)}
-                              onClickFavorite={() => {
-                                  alert('Добавили в избранное')
-                              }}/>
-                    ))}
+                    {items
+                        .filter((card) => card.name.toLowerCase().includes(searchValue))
+                        .map((card, index) => (
+                            <Card key={card.name}
+                                  title={card.name}
+                                  price={card.price}
+                                  imageURL={card.imageURL}
+                                  onClickPlus={(st, n, img) => onAddToCard(st, n, img)}
+                                  onClickFavorite={() => {
+                                      alert('Добавили в избранное')
+                                  }}/>
+                        ))}
 
                 </div>
             </div>
